2929b5f25cb6c9b029a29df9b7fc03d7c989fe3f
KristofDM <kristof.de.middelaer@gmail.com>
2014-03-08 23:43:31 +0100

    Added wolfram API call. Figuring out how to make a query with LINQ from the xml document. is wel fancy shizzle ze

--- WorkshopSushiCsharp/Form1.cs
+++ WorkshopSushiCsharp/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Xml.Linq;
 
 namespace WorkshopSushiCsharp
 {
@@ -43,6 +44,14 @@ namespace WorkshopSushiCsharp
 
         private void button1_Click(object sender, EventArgs e)
         {
+
+            //Calculate all the things! http://api.wolframalpha.com/v2/query?input=female%20population%20Belgium&appid=99LE3A-EG946R2RTA for example
+            XDocument xml = XDocument.Load("http://api.wolframalpha.com/v2/query?input=female%20population%20Belgium&appid=99LE3A-EG946R2RTA");
+            XElement test = xml.Element("queryresult");
+            var values = xml.Descendants("queryresult").Where(i => i.Element("pod").Value == "Input interpretation").Where(f => f.Element("subpod").Value == "");
+
+
+
             this.Hide();
             Form2 f2 = new Form2(textBox1.Text, "placeholder");
             f2.Show();
