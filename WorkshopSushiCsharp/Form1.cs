2873d8616a690e5a0bd5dd4ca2d1e539a72da7d4
KristofDM <kristof.de.middelaer@gmail.com>
2014-03-11 17:23:07 +0100

    Added correct query

--- WorkshopSushiCsharp/Form1.cs
+++ WorkshopSushiCsharp/Form1.cs
@@ -46,13 +46,19 @@ namespace WorkshopSushiCsharp
         {
 
             //Calculate all the things! http://api.wolframalpha.com/v2/query?input=female%20population%20Belgium&appid=99LE3A-EG946R2RTA for example
-            XDocument xml = XDocument.Load("http://api.wolframalpha.com/v2/query?input=female%20population%20Belgium&appid=99LE3A-EG946R2RTA");
+            // http://api.wolframalpha.com/v2/query?input=Belgium&appid=99LE3A-EG946R2RTA
+            XDocument xml = XDocument.Load("http://api.wolframalpha.com/v2/query?input=Belgium&appid=99LE3A-EG946R2RTA");
             XElement test = xml.Element("queryresult");
-            var values = xml.Descendants("queryresult").Where(i => i.Element("pod").Value == "Input interpretation").Where(f => f.Element("subpod").Value == "");
+            //var values = xml.Descendants("queryresult").Where(i => i.Element("pod").Value == "Input interpretation").Where(f => f.Element("subpod").Value == "");
+            var lv1s = from lv1 in xml.Descendants("pod")
+                       where (lv1.Attribute("title").Value == "Demographics")
+                       select lv1.Descendants("subpod").Descendants("plaintext").ElementAt(0).Value;
+
+            label3.Text = lv1s.ToList<string>().ElementAt(0);
+            // zomg, text is showin up
 
 
-
-            this.Hide();
+          //  this.Hide();
             Form2 f2 = new Form2(textBox1.Text, "placeholder");
             f2.Show();
         }
